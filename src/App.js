bc7d83d59f59040b73e821c4870d032dbad954f5
German Presser <germanp_89@hotmail.com>
2023-04-08 18:35:12 -0300

    guardado ultimo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
